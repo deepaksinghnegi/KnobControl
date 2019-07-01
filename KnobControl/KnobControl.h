bf5da86f3a01cc495f7135ce36bbe2bb2bd2d297
Deepak Singh Negi <bobby@Deepaks-MacBook-Air.local>
2019-07-01 23:53:20 +0530

    Initial commit

---
+++ KnobControl/KnobControl.h
@@ -0,0 +1,19 @@
+//
+//  KnobControl.h
+//  KnobControl
+//
+//  Created by Deepak Singh Negi on 01/07/19.
+//  Copyright © 2019 Deepak Singh Negi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KnobControl.
+FOUNDATION_EXPORT double KnobControlVersionNumber;
+
+//! Project version string for KnobControl.
+FOUNDATION_EXPORT const unsigned char KnobControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KnobControl/PublicHeader.h>
+
+
